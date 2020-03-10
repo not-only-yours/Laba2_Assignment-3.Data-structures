a8e781bd86cf179e3eb1edb27148b5ab50c74302
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-03-10 18:45:49 +0200

    new update (parsing)

--- src/ParseString.java
+++ src/ParseString.java
@@ -2,51 +2,49 @@ import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class ParseString {
     public static String RPN=new String();
-    public static ArrayList<Character> stek=new ArrayList<>();
+    public static Stack<Character> stack=new Stack<>();
     public static String buff=new String();
-    public static ArrayList<Pair<Integer,Character>> Multiply=new ArrayList<>();
-    public static Pair<Integer,Character> First = new Pair<>(2,'+');
-    public static Pair<Integer,Character> Second = new Pair<>(2,'-');
-    public static Pair<Integer,Character> Third = new Pair<>(2,'*');
-    public static Pair<Integer,Character> Fourth = new Pair<>(2,'/');
-    public static Pair<Integer,Character> Fifth = new Pair<>(4,'^');
     public static void parseString(){
-
-        Multiply.add(First);
-        Multiply.add(Second);
-        Multiply.add(Third);
-        Multiply.add(Fourth);
-        Multiply.add(Fifth);
-        buff = new String();
-        RPN = new String();
-        stek = new ArrayList<>();
         Scanner input = new Scanner(System.in);
                 buff= input.nextLine().replaceAll(" ","");
-        for(int i=0;i<buff.length();i++) {
-            if (!stek.isEmpty()) {
-                for (Pair<Integer, Character> m : Multiply) {
-                    int a = 0, b = 0;
-                    if (stek.get(stek.size() - 1).equals(m.getValue())) a = m.getKey();
-                    if (buff.charAt(i) == m.getValue()) b = m.getKey();
-                    if (a > b) {
-                        RPN += Character.toString(stek.get(stek.size() - 1));
-                        stek.remove(stek.size() - 1);
-                    }
-                    if (a <= b) stek.add(buff.charAt(i));
+                stack.add('0');
+        while(!stack.isEmpty()) {
+            stack.pop();
+            for (int i = 0; i < buff.length(); i++) {
+                if(Character.isDigit(buff.charAt(i))){
+                    RPN+=buff.charAt(i);
                 }
+                if(stack.isEmpty()&&!Character.isDigit(buff.charAt(i))){
+                    stack.push(buff.charAt(i));
+                }
+                if(!stack.isEmpty()) {
+                  if (buff.charAt(i) == '*' || buff.charAt(i) == '/') {
+                      if (stack.peek() == '-' || stack.peek() == '+') {
+                          stack.add(buff.charAt(i));
+                      }
+                  }
+                  if (stack.peek() == '*' || stack.peek() == '/') {
+                      if (buff.charAt(i) == '+' || buff.charAt(i) == '-') {
+                          do {
+                              RPN += stack.peek();
+                              stack.pop();
+                          } while (stack.peek() != '+' || stack.peek() != '-' || stack.isEmpty());
+                      }
+                  }
+              }
             }
-
-            if (Character.isDigit(buff.charAt(i)))
-                RPN += Character.toString(buff.charAt(i));
-            else stek.add(buff.charAt(i));
+            System.out.println(buff);
+            System.out.println(stack);
+            System.out.println(RPN);
+            do{
+                RPN+=stack.peek();
+                stack.pop();
+            }while (!stack.isEmpty());
         }
-        for(int i=stek.size()-1;i>=0;i--)RPN+=Character.toString(stek.get(i));
-        System.out.println(RPN);
-       System.out.println(Multiply);
     }
-
 }
 
